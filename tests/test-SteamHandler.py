c7f57419289b55b6387c1b9241086203253d2203
bhghdhfh <noahliby@gmail.com>
2024-02-09 15:16:44 -0500

    Fixed Tests

--- tests/test-SteamHandler.py
+++ tests/test-SteamHandler.py
@@ -1,15 +1,18 @@
 import unittest
 import sys
-sys.path.append('../Gamekit')
-import GameKit.SteamHandler
- 
-# importing
-from parentdirectory.geeks import geek_method
+import os
+# While I am ware that there is a better way of doing this, i simply dont care enough to do it as it would require reformating the entire application.
+sys.path.append(str(os.getcwd()).replace("/tests", ""))
+import SteamHandler
 
 class test_requests(unittest.TestCase):
 
-    def should_fail(self):
+    def test_should_success(self):
+        self.assertEqual(1, 1)
+
+    def test_should_fail(self):
         self.assertEqual(1,2)
+
 
 
 
