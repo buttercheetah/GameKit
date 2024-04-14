2dc1a053561f688da43533de356d2588c424ef29
bhghdhfh <noahliby@gmail.com>
2024-04-14 14:32:00 -0400

    Added some documentation to code

--- app.py
+++ app.py
@@ -1,40 +1,46 @@
+# Import necessary modules
 import requests, os, random, SteamHandler
 from json import dumps
 from flask import Flask, Response, render_template, redirect, request, send_from_directory
 from flask_apscheduler import APScheduler
 import traceback
 
+# Initialize Flask app and APScheduler
 app = Flask(__name__)
 scheduler = APScheduler()
 
+# Initialize SteamHandler and retrieve environment variables
 Steam = SteamHandler.Steam(os.environ.get('STEAM_KEY', 'Steam_api_key'))
 web_url = os.environ.get('WEB_URL', 'web_url')
 production = os.environ.get('PRODUCTION', False)
 
+# Route for default page
 @app.route('/')
 def default():
     return render_template("Search_Page_Account.html", web_url=web_url)
 
+# Route for authentication with Steam
 @app.route("/auth")
 def auth_with_steam():
     return render_template("steam_login_redirect.html",redirect_url=Steam.get_openid_url(web_url))
-    #return redirect(Steam.get_openid_url(web_url))
 
+# Route for about page
 @app.route("/about")
 def about():
     return render_template("About_Us.html")
 
+# Route for contact page
 @app.route("/contact")
 def contact():
     return render_template("Contact_Us.html")
 
+# Route for authorization
 @app.route("/authorize")
 def authorize():
     user_id=str(request.args['openid.identity']).replace('https://steamcommunity.com/openid/id/','')
-    #print(request.args)
     return render_template("steam_login_receive_redirect.html",redirect_url=f"{web_url}/user/{user_id}",user_id=user_id)
-    #return redirect(f"{web_url}/user/{str(request.args['openid.identity']).replace('https://steamcommunity.com/openid/id/','')}")
 
+# Route for search functionality
 @app.route("/search/")
 def search():
     search_query = request.args.get('search')
@@ -43,33 +49,22 @@ def search():
         return redirect(f"{web_url}/user/{search_query}")
     else:
         vanitysearch = Steam.resolve_vanity_url(search_query)
-        #print(vanitysearch)
         if vanitysearch['success'] == True:
             return redirect(f"{web_url}/user/{vanitysearch['steamid']}")
         else:
             return Response(404)
 
-#@app.route('/dev/test')
-#def test():
-#    print(Steam.get_user_summeries("76561197990263870"))
-#    return Steam.get_user_friend_list("76561198180337238")
-
+# Route for user login
 @app.route('/login')
 def login():
     return render_template("LoginPage.html")
+
+# Route for user profile
 @app.route('/user/<steamid>')
 def user(steamid):
-    # Tempory implementation
-    # will need to figure out after questions
-    #Steam.get_user_inventory(steamid)
-    #Steam.get_user_recently_played(steamid, 10)
-    #Steam.get_user_group_list(steamid)
-    #Steam.get_global_achievement_percentage(1086940)
-    #Steam.get_user_achievements_per_game(steamid, 1086940)
-    #Steam.get_user_stats_for_game(steamid, 1086940)
-    #Steam.get_user_steam_level(steamid)
-    #Steam.get_user_badges(steamid)
     return render_template("User_Page.html", user=Steam.get_user_summeries([steamid])[steamid], userlevel=Steam.get_user_steam_level(steamid), steamid = steamid)
+
+# Route for user's friends list
 @app.route('/user/<steamid>/friends')
 def friend_list(steamid):
     friends = Steam.get_user_friend_list(steamid)
@@ -77,14 +72,16 @@ def friend_list(steamid):
         return render_template("examples/friend_list_error_example.html", user=Steam.get_user_summeries([steamid])[steamid])
     friends = list(friends.values())
     return render_template("examples/friend_list_example.html", friends=friends)
+
+# Route for user's games list
 @app.route('/user/<steamid>/games')
 def game_list(steamid):
     games = Steam.get_user_owned_games(steamid)['games']
-    #print(games)
     if not games:
         return render_template("examples/game_list_error_example.html", user=Steam.get_user_summeries([steamid])[steamid])
     return render_template("examples/game_list_example.html", games=games)
 
+# Route for retrieving user's friend list via API
 @app.route("/api/friends")
 def game_api():
     steamid = request.args.get('steamid')
@@ -96,7 +93,8 @@ def game_api():
     )
     else:
         return data
-        
+
+# Route for retrieving user's groups list via API
 @app.route("/api/groups")
 def groups_api():
     try:
@@ -110,6 +108,7 @@ def groups_api():
         status=401,
     )
 
+# Route for retrieving user's achievements via API
 @app.route("/api/achievments")
 def achievments_api():
     try:
@@ -120,7 +119,6 @@ def achievments_api():
         for appid in appids:
             appachievment = Steam.get_user_achievements_per_game(steamid, appid)
             response.append(appachievment)
-            #print(appachievment,'\n\n\n')
         return {'Response':response}
     except Exception:
         traceback.print_exc()
@@ -128,6 +126,8 @@ def achievments_api():
         "Data is Private",
         status=401,
     )
+
+# Route for retrieving user's games via API
 @app.route("/api/games")
 def games_api():
     try:
@@ -139,6 +139,8 @@ def games_api():
         "Data is Private",
         status=401,
     )
+
+# Route for retrieving user's game data via API
 @app.route("/api/game_data")
 def game_data_api():
     try:
@@ -153,16 +155,22 @@ def game_data_api():
         status=401,
     )
 
+# Route for serving JavaScript files
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('templates/javascript', path)
+
+# Route for serving CSS files
 @app.route('/stylesheets/<path:path>')
 def send_stylesheets(path):
     return send_from_directory('templates/stylesheets', path)
+
+# Route for serving image files
 @app.route('/images/<path:path>')
 def send_images(path):
     return send_from_directory('templates/images', path)
 
+# Scheduler tasks for cache clearing
 @scheduler.task('interval', id='clear_cache', hours=1)
 def clear_cache():
     Steam.db_manager.clear_cache()
@@ -198,7 +206,6 @@ def clear_achievement_percentages_table():
 @scheduler.task('interval', id='clear_achievements_table', days=1)
 def clear_achievements_table():
     Steam.db_manager.clear_achievements_table()
-
 
 @app.errorhandler(404) 
 def not_found(e): 
--- templates/javascript/AccountAPIFunctions.js
+++ templates/javascript/AccountAPIFunctions.js
@@ -1,222 +1,248 @@
+// Asynchronously loads the user's friends list from the API using their Steam ID
 async function loadFriends(usteamid) {
+    // Construct the API URL for fetching friends data
     const apiUrl = `/api/friends?steamid=${usteamid}`;
 
+    // Fetch the data from the API endpoint
     fetch(apiUrl)
     .then(response => {
+        // Handle HTTP errors
         if (!response.ok) {
+            // Display an error message if the response is not okay
             document.getElementById("friends_list").innerHTML = `<p>Private</p>`;
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        // Parse the response as JSON
         return response.json();
     })
     .then(data => {
-        // Iterate over each friend in the response
+        // Display the list of friends on the webpage
         document.getElementById("friends_list").innerHTML = `<ul>`;
         let i = 0;
+        // Iterate over each friend in the response
         for (const steamid in data) {
             i++;
-            if (i === 9) { break; }
+            if (i === 9) { break; } // Limit the number of displayed friends
             if (data.hasOwnProperty(steamid)) {
                 const friend = data[steamid];
-                // Log or display the formatted output (avatar - personaname)
-                //console.log(`${friend.avatar} - ${friend.personaname}`);
+                // Display each friend's name with a link to their profile
                 document.getElementById("friends_list").innerHTML += `<li><a href='/user/${steamid}'>${friend.personaname}</a></li>`;
             }
         }
         document.getElementById("friends_list").innerHTML += `</ul>`;
     })
     .catch(error => {
+        // Handle any errors that occur during the fetch operation
         console.error('Error:', error);
     });
 }
+
+// Asynchronously loads the user's groups from the API using their Steam ID
 async function loadGroups(usteamid) {
+    // Construct the API URL for fetching groups data
     const apiUrl = `/api/groups?steamid=${usteamid}`;
 
+    // Fetch the data from the API endpoint
     fetch(apiUrl)
     .then(response => {
+        // Handle HTTP errors
         if (!response.ok) {
+            // Display an error message if the response is not okay
             document.getElementById("user_groups").innerHTML = `<p>Private</p>`;
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        // Parse the response as JSON
         return response.json();
     })
     .then(data => {
-        // Iterate over each friend in the response
+        // Display the list of groups on the webpage
         document.getElementById("user_groups").innerHTML = `<ul>`;
         let i = 0;
-        let done = []
+        let done = []; // Track group IDs to avoid duplicates
+        // Iterate over each group in the response
         for (const allallgroups in data.Response) {
             i++;
             for (const allgroups in allallgroups) {
-                
-                if (i === 10) { break; }
+                if (i === 10) { break; } // Limit the number of displayed groups
                 const groups = data.Response.groups[i];
                 for (const group in groups[0]) {
                     const cgroup = groups[0][group][0]
                     if (done.includes(cgroup['groupid'])) {
-                        
+                        // Skip if the group has already been displayed
                     } else {
-                        console.log(cgroup);
+                        // Display the group name
                         document.getElementById("user_groups").innerHTML += `<li>${cgroup.groupName}</li>`;
-                        done.push(cgroup['groupid'])
-                
+                        done.push(cgroup['groupid']) // Mark the group as displayed
                     }
-           
                 }
             }
-            // Log or display the formatted output (avatar - personaname)
-            //console.log(`${friend.avatar} - ${friend.personaname}`);
-            //document.getElementById("user_groups").innerHTML += `<li><a href='/user/${steamid}'>${friend.personaname}</a></li>`;
         }
         document.getElementById("user_groups").innerHTML += `</ul>`;
     })
     .catch(error => {
+        // Handle any errors that occur during the fetch operation
         console.error('Error:', error);
     });
 }
+
+// Asynchronously loads the user's games library from the API using their Steam ID
 async function loadGames(usteamid) {
+    // Construct the API URL for fetching games data
     const apiUrl = `/api/games?steamid=${usteamid}`;
+    
+    // Fetch the data from the API endpoint
     fetch(apiUrl)
     .then(response => {
+        // Handle HTTP errors
         if (!response.ok) {
-        document.getElementById("friends_list").innerHTML = `<p>Private</p>`;
-        throw new Error(`HTTP error! Status: ${response.status}`);
+            // Display an error message if the response is not okay
+            document.getElementById("friends_list").innerHTML = `<p>Private</p>`;
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        // Parse the response as JSON
         return response.json();
     })
     .then(data => {
-        console.log(data)
+        // Sort games alphabetically by name
         data.games.sort((a, b) => a.name.localeCompare(b.name));
         let gamelist = "";
+        // Iterate over each game in the response
         for (let i = 0; i < data.games.length; i++) {
             const game = data.games[i];
-            //console.log(game);
-            //console.log(`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg - ${game.name} - ${game.playtime_forever}`);
+            // Add a link to select the game and display its name
             gamelist += `<a onclick="selectgame('${game.appid}','${game.name}','${usteamid}')">${game.name}</a>`;
         }
         document.getElementById("gamedropdown-list").innerHTML = gamelist;
-        // Iterate over each friend in the response
+
+        // Display the list of games on the webpage
         document.getElementById("games_list").innerHTML = `<ul>`;
         let i = 0;
         for (const gamedata in data['games']) {
             i++;
-            if (i == 8) { break; }
+            if (i == 8) { break; } // Limit the number of displayed games
             const game = data['games'][gamedata];
-            //console.log(`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg - ${game.name}`);
+            // Display each game's name
             document.getElementById("games_list").innerHTML += `<li>${game.name}</li>`;
         }
         document.getElementById("games_list").innerHTML += `</ul>`;
 
+        // Sort games by playtime and display the top 4 most played games
         const sortedGames = data['games'].sort((a, b) => b.playtime_forever - a.playtime_forever);
-        // Display the top 10 most played games
         document.getElementById("most_played_games").innerHTML = `<ol>`;
-        for (let i = 0; i < Math.min(3, sortedGames.length); i++) {
+        for (let i = 0; i < Math.min(4, sortedGames.length); i++) {
             const game = sortedGames[i];
-            //console.log(`http://media.steampowered.com/steamcommunity/public/images/apps/${game.appid}/${game.img_icon_url}.jpg - ${game.name} - ${game.playtime_forever}`);
             document.getElementById("most_played_games").innerHTML += `<li>${Math.floor(game.playtime_forever/60)} hours - ${game.name}</li>`;
         }
         document.getElementById("most_played_games").innerHTML += `</ol>`;
 
+        // Fetch achievements for the top 50 most recently played games
         const topsortedGames = data['games']
         .sort((a, b) => b.rtime_last_played - a.rtime_last_played)
         .slice(0, 50)
         .map(game => game.appid)
         .join(',');
 
-        //console.log(topsortedGames);
         const apiUrl = `/api/achievments?steamid=${usteamid}&appids=${topsortedGames}`;
         
         fetch(apiUrl)
         .then(response => {
+            // Handle HTTP errors
             if (!response.ok) {
-            document.getElementById("newest_achievement").innerHTML = `<p>Private</p>`;
-            throw new Error(`HTTP error! Status: ${response.status}`);
+                // Display an error message if the response is not okay
+                document.getElementById("newest_achievement").innerHTML = `<p>Private</p>`;
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
+            // Parse the response as JSON
             return response.json();
         })
         .then(data => {
-            
             // Filter only achievements that have been achieved
             const filteredData = data.Response.map(item => {
                 if (item.playerstats && item.playerstats.achievements) {
-                const achievements = item.playerstats.achievements.filter(achievement => achievement.achieved === 1);
-                return { playerstats: { achievements: achievements }, ...item.playerstats }
+                    const achievements = item.playerstats.achievements.filter(achievement => achievement.achieved === 1);
+                    return { playerstats: { achievements: achievements }, ...item.playerstats }
                 } else {
-                return item;
+                    return item;
                 }
             });
-            //console.log(filteredData);
             
             // Sorting the achievements by unlocktime in descending order
             responseData = filteredData.flat().filter(data => data.playerstats?.achievements?.length > 0);
             responseData.sort((a, b) => b.playerstats.achievements[0].unlocktime - a.playerstats.achievements[0].unlocktime);
 
-            // Creating the ordered list HTML
+            // Create an ordered list HTML
             let ol = document.createElement('ol');
 
-            // Iterating through the achievements and adding them to the list
+            // Add achievements to the list
             for (let i = 0; i < Math.min(responseData.length, 5); i++) {
                 let achievement = responseData[i].playerstats.achievements[0];
-                let gameName = responseData[i].gameName; // Accessing gameName property here
-                let displayName = achievement.displayName; // Accessing gameName property here
-                let apiname = achievement.apiname;
-                let unlocktime = new Date(achievement.unlocktime * 1000).toLocaleDateString(); // Converting Unix timestamp to readable date
+                let gameName = responseData[i].gameName;
+                let displayName = achievement.displayName;
+                let unlocktime = new Date(achievement.unlocktime * 1000).toLocaleDateString();
             
                 let li = document.createElement('li');
                 li.textContent = `${gameName} - ${displayName} - ${unlocktime}`;
                 ol.appendChild(li);
             }
 
-            // Setting the inner HTML of the div with ID "newest_achievement"
-
+            // Set the inner HTML of the div with ID "newest_achievement"
             document.getElementById('newest_achievement').innerHTML = '';
             document.getElementById('newest_achievement').appendChild(ol);
-            
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-            
         })
+        .catch(error => {
+            // Handle any errors that occur during the fetch operation
+            console.error('Error:', error);
+        });
+    })
     .catch(error => {
+        // Handle any errors that occur during the fetch operation
         console.error('Error:', error);
     });
 }
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// Toggle visibility of the dropdown menu
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  function filterFunction() {
+}
+
+// Filter items in the dropdown menu based on user input
+function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     div = document.getElementById("myDropdown");
     a = div.getElementsByTagName("a");
     for (i = 0; i < a.length; i++) {
-      txtValue = a[i].textContent || a[i].innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        a[i].style.display = "";
-      } else {
-        a[i].style.display = "none";
-      }
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
     }
-  }
+}
+
+// Update selected game information
 function selectgame(appid,appname,steamid) {
     document.getElementById("GameSelection").innerHTML = appname;
     appidselection = appid;
 }
+
+// Load additional data for a selected game
 function loaddataforgame(steamid, operation) {
-    console.log(appidselection)
+    // Set the databox to a loading statement.
+    document.getElementById("DataBoxData").innerHTML  = "<p>Loading...</p>";
+    //console.log(appidselection)
     const apiUrl = `/api/game_data?appid=${appidselection}&steamid=${steamid}`;
     fetch(apiUrl)
     .then(response => {
+        // Handle HTTP errors
         if (!response.ok) {
-        document.getElementById("friends_list").innerHTML = `<p>Private</p>`;
-        throw new Error(`HTTP error! Status: ${response.status}`);
+            // Display an error message if the response is not okay
+            document.getElementById("friends_list").innerHTML = `<p>Private</p>`;
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        // Parse the response as JSON
         return response.json();
     })
     .then(data => {
@@ -224,12 +250,10 @@ function loaddataforgame(steamid, operation) {
         try {
             if (operation == 'Last_Played') {
                 let date = new Date(data.rtime_last_played * 1000);
-                console.log(date);
-                console.log(data.rtime_last_played);
                 texttoset = `<p>Last played on ${date}</p>`;
             } else if (operation == 'Achievements') {
                 for (let i = 0; i < data.Achievments.playerstats.achievements.length; i++) {
-                        texttoset += `<p>${data.Achievments.playerstats.achievements[i]['displayName']} - ${data.Achievments.playerstats.achievements[i]['description']}</p>`;
+                    texttoset += `<p>${data.Achievments.playerstats.achievements[i]['displayName']} - ${data.Achievments.playerstats.achievements[i]['description']}</p>`;
                 }
             } else if (operation == 'Total_Playtime') {
                 texttoset = `<p>Total Playtime: ${(data.playtime_forever/60).toFixed(2)} hours</p>`;
@@ -239,10 +263,12 @@ function loaddataforgame(steamid, operation) {
             texttoset = '<p>No data to display</p>'
         }
         document.getElementById("DataBoxData").innerHTML  = texttoset;
-        console.log(data)
     })
     .catch(error => {
+        // Handle any errors that occur during the fetch operation
         console.error('Error:', error);
     });
 }
+
+// Global variable to store the selected game's App ID
 let appidselection = 0;
